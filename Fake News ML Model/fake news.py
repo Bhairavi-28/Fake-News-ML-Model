3610c2807ffa57529f297d7c10ff748d3a39656d
Bhairavi-28 <bhandaririddhima8@gmail.com>
2025-08-22 06:30:34 +0530

    Add files via upload

---
+++ Fake News ML Model/fake news.py
@@ -0,0 +1,18 @@
+### This the fist project for Athena Awards
+### FAKE NEWS DETECTOR
+
+### Import required Libraries
+import pandas as pd
+import numpy as np 
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+### Dataset to be used
+
+### Train Data 
+
+### Confusion Matrix
+
+### Test Data
