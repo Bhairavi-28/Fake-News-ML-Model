692bc93129a2911b8a70dc6fa0c440e270cb55b8
Bhairavi <bhandaririddhima8@gmail.com>
2025-10-31 17:09:38 +0530

    Refactor predict function and improve response messages
    Updated the predict function to return a more user-friendly message and fixed the JSON request method.

--- app.py
+++ app.py
@@ -3,25 +3,27 @@ import joblib
 
 app = Flask(__name__)
 
-model = joblib.load('fake_news_model.pkl')
-vectorizer = joblib.load('tfidf_vectorizer.pkl')
+# Load model + vectorizer
+model = joblib.load("fake_news_model.pkl")
+vectorizer = joblib.load("tfidf_vectorizer.pkl")
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/predict', methods=['POST'])
+@app.route("/predict", methods=["POST"])
 def predict():
-    data = request.get.json()
+    data = request.get_json()
     headline = data.get("headline", "")
 
     if not headline.strip():
-        return jsonify({"Result": "No headline provided. Please enter a valid headline."})
-    headline_tfdif = vectorizer.transform([headline])
-    prediction = model.predict(headline_tfdif)[0]
-    result = "Fake News" if prediction == 0 else "Real News"
+        return jsonify({"result": "Please enter a headline."})
 
-    return jsonify({"Result": result})
+    headline_tfidf = vectorizer.transform([headline])
+    prediction = model.predict(headline_tfidf)[0]
+    result = "Fake News" if prediction == 0 else "True News"
 
-if __name__ == '__main__':
+    return jsonify({"result": result})
+
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
